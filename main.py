c21fbf262f77a791057ddb4f3d74e771023289e8
Lei00764 <2718068057@qq.com>
2023-02-10 13:01:49 +0800

    haha

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
-from operator import mul
+import time
 
-def Add(a, b):
-    return a + b
+def display_time(func):
+    def wrapper(*args):  # 默认名称
+        t1 = time.time()
+        result = func(*args)
+        t2 = time.time()
+        print("Total time: ", t2 - t1)
+        return result
+    
+    return wrapper
 
-lambda a, x, b: a * x + b
 
-print(print(1))
+def is_prime(num):
+    if num < 2:
+        return False
+    elif num == 2:
+        return True
+    else:
+        for i in range(2, num):
+            if (num % i == 0):
+                return False
+        return True
 
-print(Add(2, 3)) 
+@display_time  # 装饰器
+def count_prime_nums(n):
+    count = 0
+    for i in range(2, n):
+        if is_prime(i) == True:
+            count += 1
+    return count
+
+print(count_prime_nums(10000))
